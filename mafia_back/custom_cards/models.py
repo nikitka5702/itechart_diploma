59a7d32d8693874c14458be2edc034b020a51420
{artimendor} <{artimendor@yandex.ru}>
2019-04-15 20:34:12 +0300

    cameCase to snake_case

--- mafia_back/custom_cards/models.py
+++ mafia_back/custom_cards/models.py
@@ -4,7 +4,7 @@ from mafia_back.settings import CARD_PIXEL_HEIGHT, CARD_PIXEL_WIDTH
 
 class SimpleCardSet(models.Model):
 
-    pieceHabitat = models.ImageField(
+    piece_habitat = models.ImageField(
         width_field=CARD_PIXEL_WIDTH,
         height_field=CARD_PIXEL_HEIGHT,
         upload_to=f'uploads/cards/%Y/%m/%d/'
@@ -22,7 +22,7 @@ class ExtendedCardSet(SimpleCardSet):
         height_field=CARD_PIXEL_HEIGHT,
         upload_to=f'uploads/cards/%Y/%m/%d/'
     )  # policeman. He checks players at night. If he found mafia he will be know that
-    loveMaker = models.ImageField(
+    love_maker = models.ImageField(
         width_field=CARD_PIXEL_WIDTH,
         height_field=CARD_PIXEL_HEIGHT,
         upload_to=f'uploads/cards/%Y/%m/%d/'
