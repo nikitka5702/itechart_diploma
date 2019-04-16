37907c5b077627d684e7cee5ea0e1cd547e7381e
nikitka5702 <niksav98@gmail.com>
2019-04-16 15:30:57 +0300

    Add react-apollo and set it up for JWT token

--- mafia-front/src/App.js
+++ mafia-front/src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize'
+
+import './App.css'
+
+import Header from './components/layout/Header'
+import Main from './components/layout/Main'
+import Footer from './components/layout/Footer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <Fragment>
+        <header>
+          <Header />
         </header>
-      </div>
+        <main>
+          <Main />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </Fragment>
     );
   }
 }
--- mafia-front/src/index.js
+++ mafia-front/src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost'
+import ApolloProvider from 'react-apollo'
+import createHttpLink from 'apollo-link-http'
+import setContext from 'apollo-link-context'
+import InMemoryCache from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from './App'
+
+import './index.css'
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token')
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        }
+    }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+})
+
+
+const app = (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'));
