1b0c18674713ac2f26a19a1baf7217ce0384a709
mikedreyeu <mikedreyeu@gmail.com>
2019-05-02 23:45:11 +0300

    Move video streaming logic into Game component

--- mafia-front/src/components/pages/Game.jsx
+++ mafia-front/src/components/pages/Game.jsx
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 import { client } from '../../index.js'
 import PlayerCard from '../elements/PlayerCard'
 
+
 const GET_MY_ID_QUERY = gql`
 query {
   me {
@@ -10,241 +11,246 @@ query {
   }
 }`
 
-var playerId;
-var gameId = 1;
-
-var mediaConstraints = {
-  audio: true,
-  video: true
-};
-
-var peerConnections = {};
-
-const signalingSocket = new WebSocket('ws://localhost:8000/ws/game/');
-
-signalingSocket.addEventListener('message', (event) => {
-  let msg = JSON.parse(event.data)
-  console.log(msg)
-  switch(msg.type) {
-    case 'player-joined':
-      handlePlayerJoinedMsg(msg);
-      break;
-    case 'player-disconnected':
-      handlePlayerDisconnectedMsg(msg);
-      break;
-    case 'video-offer':
-      handleVideoOfferMsg(msg);
-      break;
-    case 'video-answer':
-      handleVideoAnswerMsg(msg);
-      break;
-    case 'new-ice-candidate':
-      handleNewICECandidateMsg(msg);
-      break;
-    
-    default:
-  }
-});
-
-
-var handlePlayerJoinedMsg = (msg) => {
-  createPeerConnection(msg.player_id);
-  navigator.mediaDevices.getUserMedia(mediaConstraints)
-    .then(function(localStream) {
-      localStream.getTracks().forEach(track => peerConnections[msg.player_id].addTrack(track, localStream));
-    })
-    .catch(handleGetUserMediaError);
-}
-
-var handlePlayerDisconnectedMsg = (msg) => {
-  endVideoStreaming(msg.player_id);
-}
-
-var handleVideoOfferMsg = (msg) => {
-  if (peerConnections[msg.player_id]) {
-    console.error('peerConnection already exists!');
-    return;
-  }
-
-  var localStream = null;
-  createPeerConnection(msg.player_id);
-  peerConnections[msg.player_id].onnegotiationneeded = null;
-
-  var desc = new RTCSessionDescription(msg.sdp);
-
-  peerConnections[msg.player_id].setRemoteDescription(desc).then(() => {
-    return navigator.mediaDevices.getUserMedia(mediaConstraints);
-  })
-  .then((stream) => {
-    localStream = stream;
-    localStream.getTracks().forEach(track => peerConnections[msg.player_id].addTrack(track, localStream));
-  })
-  .then(() => {
-    return peerConnections[msg.player_id].createAnswer();
-  })
-  .then((answer) => {
-    return peerConnections[msg.player_id].setLocalDescription(answer);
-  })
-  .then(() => {
-    sendToServer({
-      type: 'video-answer',
-      game_id: msg.game_id,
-      player_id: playerId,
-      target_id: msg.player_id,
-      sdp: peerConnections[msg.player_id].localDescription
-    });
-  })
-  .catch(handleGetUserMediaError);
-}
-
-var handleVideoAnswerMsg = (msg) => {
-  var desc = new RTCSessionDescription(msg.sdp);
-  peerConnections[msg.player_id].setRemoteDescription(desc)
-    .then(() => {
-      console.log('setRemoteDescription(answer) succsess in promise');
-    })
-    .catch(handleError);
-}
-
-var handleNewICECandidateMsg = (msg) => {
-  var candidate = new RTCIceCandidate(msg.candidate);
-
-  peerConnections[msg.player_id].addIceCandidate(candidate)
-    .then(() => {
-      console.log('addIceCandidate(candidate) success in promise');
-    })
-    .catch(handleError);
-}
-
-
-var createPeerConnection = (targetId) => {
-  console.log('creating new peer');
-
-  let peerConnection = new RTCPeerConnection({
-      iceServers: [
-        {
-          urls: 'stun:stun3.l.google.com:19302'
-        }
-      ]
-  });
-  
-  peerConnection.onicecandidate = (event) => {
-    return handleICECandidateEvent(event, targetId);
-  }
-  peerConnection.ontrack = (event) => {
-    return handleTrackEvent(event, targetId); 
-  }
-  peerConnection.onnegotiationneeded = () => {
-    return handleNegotiationNeededEvent(targetId);
-  }
-  
-  peerConnections[targetId] = peerConnection;
-}
-
-var handleICECandidateEvent = (event, targetId) => {
-  console.log('handleICECandidateEvent triggered')
-  if (event.candidate) {
-    sendToServer({
-      type: 'new-ice-candidate',
-      game_id: gameId,
-      player_id: playerId,
-      target_id: targetId,
-      candidate: event.candidate
-    });
-  }
-}
-
-var handleNegotiationNeededEvent = (targetId) => {
-  console.log('handleNegotiationNeededEvent triggered')
-  peerConnections[targetId].createOffer().then((offer) => {
-    return peerConnections[targetId].setLocalDescription(offer);
-  })
-  .then(function() {
-    sendToServer({
-      type: 'video-offer',
-      game_id: gameId,
-      player_id: playerId,
-      target_id: targetId,
-      sdp: peerConnections[targetId].localDescription
-    });
-  })
-  .catch(handleError);
-}
-
-var handleTrackEvent = (event, playerId) => {
-  console.log('handleTrackEvent triggered')
-  document.getElementById('video-'.concat(playerId)).srcObject = event.streams[0];
-}
-
-
-var endVideoStreaming = (playerId) => {
-  let playerVideo = document.getElementById("video-" + playerId);
-
-  if (playerVideo.srcObject) {
-    playerVideo.srcObject.getTracks().forEach(track => track.stop());
-  }
-
-  peerConnections[playerId].close()
-  delete peerConnections[playerId];
-}
-
-var sendToServer = (msg) => {
-  signalingSocket.send(JSON.stringify(msg));
-}
-
-
-var handleGetUserMediaError = (error) => {
-  switch(error.name) {
-    case 'NotFoundError':
-      alert('Unable to open your call because no camera and/or microphone were found.');
-      break;
-    case 'SecurityError':
-    case 'PermissionDeniedError':
-      break;
-    default:
-      alert('Error opening your camera and/or microphone: ' + error.message);
-      break;
-  }
-}
-
-var handleError = (error) => {
-  console.error('Error ' + error.name + ': ' + error.message);
-}
 
 export default class Game extends Component {
+  playerId;
+  gameId = 1;
+
+  mediaConstraints = {
+    audio: true,
+    video: true
+  };
+
+  peerConnections = {};
+
   constructor(props) {
     super(props);
-    this.playerCardRefs = {}; // tmp
+    this.playerCardRefs = {};
     for(let i = 0; i < 10; i++) { 
       this.playerCardRefs[i] = { ref: React.createRef() };
     }
 
+    this.signalingSocket = new WebSocket('ws://localhost:8000/ws/game/');
 
-    client
-    .query({
-      query: GET_MY_ID_QUERY,
-    })
-    .then((obj) => { 
-      playerId = parseInt(obj.data.me.id);
-
-      sendToServer({
-        type: 'player-joined',
-        game_id: gameId,
-        player_id: playerId
-      });
+    this.signalingSocket.addEventListener('message', (event) => {
+      let msg = JSON.parse(event.data)
+      console.log(msg)
+      switch(msg.type) {
+        case 'player-joined':
+          this.videoHandlePlayerJoinedMsg(msg);
+          break;
+        case 'player-disconnected':
+          this.videoHandlePlayerDisconnectedMsg(msg);
+          break;
+        case 'video-offer':
+          this.videoHandleVideoOfferMsg(msg);
+          break;
+        case 'video-answer':
+          this.videoHandleVideoAnswerMsg(msg);
+          break;
+        case 'new-ice-candidate':
+          this.videoHandleNewICECandidateMsg(msg);
+          break;
+        
+        default:
+      }
     });
 
-    let cardRefs = this.playerCardRefs;
-    navigator.mediaDevices.getUserMedia(mediaConstraints)
-      .then(function(localStream) {
-        cardRefs[playerId].ref.current.videoRef.current.srcObject = localStream;
-        cardRefs[playerId].ref.current.videoRef.current.muted = true;
-      })
-      .catch(handleGetUserMediaError);
+    this.signalingSocket.addEventListener('open', (event) => {
+      client
+        .query({
+          query: GET_MY_ID_QUERY,
+        })
+        .then((obj) => { 
+          this.playerId = parseInt(obj.data.me.id);
+
+          this.videoSendToServer({
+            type: 'player-joined',
+            game_id: this.gameId,
+            player_id: this.playerId
+          });
+        });
+
+      let cardRefs = this.playerCardRefs;
+      navigator.mediaDevices.getUserMedia(this.mediaConstraints)
+        .then((localStream) => {
+          cardRefs[this.playerId].ref.current.videoRef.current.srcObject = localStream;
+          cardRefs[this.playerId].ref.current.videoRef.current.muted = true;
+        })
+        .catch(this.videoHandleGetUserMediaError);
+    });
   }
 
-  test() {
-    console.log(peerConnections)
+  videoHandlePlayerJoinedMsg = (msg) => {
+    this.videoCreatePeerConnection(msg.player_id);
+    navigator.mediaDevices.getUserMedia(this.mediaConstraints)
+      .then((localStream) => {
+        localStream.getTracks().forEach(track => this.peerConnections[msg.player_id].addTrack(track, localStream));
+      })
+      .catch(this.videoHandleGetUserMediaError);
+  }
+
+  videoHandlePlayerDisconnectedMsg = (msg) => {
+    this.videoEndVideoStreaming(msg.player_id);
+  }
+
+  videoHandleVideoOfferMsg = (msg) => {
+    if (this.peerConnections[msg.player_id]) {
+      console.error('peerConnection already exists!');
+      return;
+    }
+  
+    var localStream = null;
+    this.videoCreatePeerConnection(msg.player_id);
+    this.peerConnections[msg.player_id].onnegotiationneeded = null;
+  
+    var desc = new RTCSessionDescription(msg.sdp);
+  
+    this.peerConnections[msg.player_id].setRemoteDescription(desc).then(() => {
+      return navigator.mediaDevices.getUserMedia(this.mediaConstraints);
+    })
+    .then((stream) => {
+      localStream = stream;
+      localStream.getTracks().forEach(track => this.peerConnections[msg.player_id].addTrack(track, localStream));
+    })
+    .then(() => {
+      return this.peerConnections[msg.player_id].createAnswer();
+    })
+    .then((answer) => {
+      return this.peerConnections[msg.player_id].setLocalDescription(answer);
+    })
+    .then(() => {
+      this.videoSendToServer({
+        type: 'video-answer',
+        game_id: msg.game_id,
+        player_id: this.playerId,
+        target_id: msg.player_id,
+        sdp: this.peerConnections[msg.player_id].localDescription
+      });
+    })
+    .catch(this.videoHandleGetUserMediaError);
+  }  
+
+  videoHandleVideoAnswerMsg = (msg) => {
+    var desc = new RTCSessionDescription(msg.sdp);
+    this.peerConnections[msg.player_id].setRemoteDescription(desc)
+      .then(() => {
+        console.log('setRemoteDescription(answer) succsess in promise');
+      })
+      .catch(this.videoHandleError);
+  }
+
+  videoHandleNewICECandidateMsg = (msg) => {
+    var candidate = new RTCIceCandidate(msg.candidate);
+  
+    this.peerConnections[msg.player_id].addIceCandidate(candidate)
+      .then(() => {
+        console.log('addIceCandidate(candidate) success in promise');
+      })
+      .catch(this.videoHandleError);
+  }
+
+  videoCreatePeerConnection = (targetId) => {
+    console.log('creating new peer');
+  
+    let peerConnection = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: 'stun:stun3.l.google.com:19302'
+          }
+        ]
+    });
+    
+    peerConnection.onicecandidate = (event) => {
+      return this.videoHandleICECandidateEvent(event, targetId);
+    }
+    peerConnection.ontrack = (event) => {
+      return this.videoHandleTrackEvent(event, targetId); 
+    }
+    peerConnection.onnegotiationneeded = () => {
+      return this.videoHandleNegotiationNeededEvent(targetId);
+    }
+    
+    this.peerConnections[targetId] = peerConnection;
+  }
+
+  videoHandleICECandidateEvent = (event, targetId) => {
+    console.log('videoHandleICECandidateEvent triggered')
+    if (event.candidate) {
+      this.videoSendToServer({
+        type: 'new-ice-candidate',
+        game_id: this.gameId,
+        player_id: this.playerId,
+        target_id: targetId,
+        candidate: event.candidate
+      });
+    }
+  }
+
+  videoHandleNegotiationNeededEvent = (targetId) => {
+    console.log('videoHandleNegotiationNeededEvent triggered');
+    this.peerConnections[targetId].createOffer().then((offer) => {
+      return this.peerConnections[targetId].setLocalDescription(offer);
+    })
+    .then(() => {
+      this.videoSendToServer({
+        type: 'video-offer',
+        game_id: this.gameId,
+        player_id: this.playerId,
+        target_id: targetId,
+        sdp: this.peerConnections[targetId].localDescription
+      });
+    })
+    .catch(this.videoHandleError);
+  }
+
+  videoHandleTrackEvent = (event, playerId) => {
+    console.log('videoHandleTrackEvent triggered')
+    document.getElementById('video-'.concat(playerId)).srcObject = event.streams[0];
+  }
+
+  videoEndVideoStreaming = (playerId) => {
+    let playerVideo = document.getElementById("video-" + playerId);
+  
+    if (playerVideo.srcObject) {
+      playerVideo.srcObject.getTracks().forEach(track => track.stop());
+    }
+  
+    this.peerConnections[playerId].close()
+    delete this.peerConnections[playerId];
+  }
+
+  videoSendToServer = (msg) => {
+    this.signalingSocket.send(JSON.stringify(msg));
+  }
+
+  videoHandleGetUserMediaError = (error) => {
+    switch(error.name) {
+      case 'NotFoundError':
+        alert('Unable to open your call because no camera and/or microphone were found.');
+        break;
+      case 'SecurityError':
+      case 'PermissionDeniedError':
+        break;
+      default:
+        alert('Error opening your camera and/or microphone: ' + error.message);
+        break;
+    }
+  }
+
+  videoHandleError = (error) => {
+    console.error('Error ' + error.name + ': ' + error.message);
+  }
+
+  componentWillUnmount() {
+    if (this.signalingSocket.readyState === WebSocket.OPEN) {
+      this.signalingSocket.onclose = () => {}
+      this.signalingSocket.close()
+    }
+  }
+
+  testGetPeers = () => {
+    console.log(this.peerConnections)
   }
 
   render() {
@@ -275,7 +281,7 @@ export default class Game extends Component {
                 <p>Alert</p>
                 <p>Info</p>
                 <p>Other functionality</p>
-                <button className='btn waves-effect waves-light' onClick={this.test}>Test</button>
+                <button className='btn waves-effect waves-light' onClick={this.testGetPeers}>Get peers</button>
               </div>
             </div>
           </div>
